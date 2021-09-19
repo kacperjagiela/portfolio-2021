764878402648967fb50543afec14506a2aaa36a9
kacperjagiela <jagielakacper80@gmail.com>
2021-09-19 20:49:12 +0200

    config: add prettierignore and eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,6 @@ module.exports = {
         'prettier', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
-    ignorePatterns: ['.next/*'],
     rules: {
         'sort-imports': 'off',
         'import/order': 'off',
